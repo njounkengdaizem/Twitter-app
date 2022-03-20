fe6e3093095c727022830e5361d798003db0fa99
Merville Njounkeng Daize <njounkengdaizem@berea.edu>
2022-03-20 02:02:40 -0400

    Added git file

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.kt
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.codepath.apps.restclienttemplate.models.Tweet
 
+
 class TweetsAdapter(val tweets: ArrayList<Tweet>) : RecyclerView.Adapter<TweetsAdapter.ViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TweetsAdapter.ViewHolder {
         val context = parent.context
